c542c09c7bc4f5902127994734499c5bf09a2f33
Riccardo Traverso <rtraverso86@gmail.com>
2013-05-16 14:52:03 +0200

    Data.Set.everything -> Data.Set.all
    Renamed 'Data.Set.everything' in 'all' by hiding Prelude's equivalent
    which works on lists.
    This way it is more easier to relate to the meaning of the function when
    importing  Haskkit.Data.Set.

--- Haskkit/Data/Set.hs
+++ Haskkit/Data/Set.hs
@@ -1,12 +1,14 @@
 -- | Extensions for Data.Set libraries.
 module Haskkit.Data.Set (
-      everything
+      all
     ) where
 
+import Prelude hiding (all)
 import Data.Set (Set)
 import qualified Data.Set as S
+
     
--- | @'everything' p s@ returns 'True' if and only if all elements in 'Set' @s@
+-- | @'all' p s@ returns 'True' if and only if all elements in 'Set' @s@
 -- satisfy predicate @p@.
-everything :: Ord a => (a -> Bool) -> Set a -> Bool
-everything p = S.null . S.filter (not . p)
+all :: Ord a => (a -> Bool) -> Set a -> Bool
+all p = S.null . S.filter (not . p)
