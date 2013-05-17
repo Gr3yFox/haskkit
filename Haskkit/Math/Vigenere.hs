c806b8ea0d13e6e8ac22b24045bd990f73daf8c3
Riccardo Traverso <rtraverso86@gmail.com>
2013-05-17 16:12:14 +0200

    Vigenere: documentation and cleanup imports
    Added some quick documentation in the Vigenere cipher module.
    Removed old unused imports.

--- Haskkit/Math/Vigenere.hs
+++ Haskkit/Math/Vigenere.hs
@@ -7,8 +7,8 @@ module Haskkit.Math.Vigenere (
     , caesar
     ) where
 
-import Data.List (cycle, elemIndex)
-import Data.Char (ord, chr, isAscii, isAlpha)
+import Data.List (cycle)
+import Data.Char (ord, chr)
 import Haskkit.Data.Char
 
 data Operation = Encode | Decode deriving (Show, Eq)
@@ -24,12 +24,20 @@ encode op k c = let iR = getOp op (alphaIndex c) (alphaIndex k)
 loopKey :: String -> String
 loopKey = cycle . filter isAsciiAlpha 
 
+-- | Takes in input a secret key, an 'Operation', and a (possibly encrypted) text and
+-- applies the Vigenere cipher.
+-- Only the letters in the ASCII range from the secret key are actually used.
+-- All other characters, both from the key and from the text, are ignored, as well as
+-- the letter case.
 vigenere :: String -> Operation -> String -> String
 vigenere key op text = go (loopKey key) text
     where go _ [] = []
           go (k:ks) (x:xs) | isAsciiAlpha x = encode op k x : go ks xs
                            | otherwise      = x : go (k:ks) xs
 
+-- | Takes in input a secret key, an 'Operation', and a (possibly encrypted) text and
+-- applies the Caesar cipher.
+-- Only letters in the ASCII range from are valid keys.
 caesar :: Char -> Operation -> String -> String
 caesar k op = map (encode' op k)
     where encode' op k c = if isAsciiAlpha c then encode op k c else c
